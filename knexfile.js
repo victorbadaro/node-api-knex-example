eaaa48344a675b2c35d90bbe53f581b1ff0b53ff
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-28 08:13:16 -0300

    chore: add migrations directory to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 module.exports = {
   development: {
@@ -7,6 +8,9 @@ module.exports = {
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     }
   }
 };
