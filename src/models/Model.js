0ccaadd391aa0b328e1b428bef22ee1fc7b961fb
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-28 16:46:31 -0300

    feat: add Model class

---
+++ src/models/Model.js
@@ -0,0 +1,25 @@
+import knex from '../database';
+
+class Model {
+    constructor(table) {
+        this.table = table;
+    }
+
+    async find() {
+
+    }
+
+    async create() {
+
+    }
+
+    async update() {
+
+    }
+
+    async delete() {
+
+    }
+}
+
+export { Model };
