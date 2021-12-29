8cb541c6a720a8448e763d7af5a310e501349137
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-28 23:43:17 -0300

    feat: add methods to find, create, update and delete products

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,30 +2,64 @@ import knex from '../database';
 
 class ProductController {
     async index(request, response) {
-        return response.json();
+        const products = await knex('products').orderBy('id');
+
+        return response.json(products);
     }
 
     async find(request, response) {
         const { id } = request.params;
+        const product = await knex('products').where({ id }).limit(1);
 
-        return response.json();
+        if (product.length === 0)
+            return response.status(400).json({ error: 'Product not found!' });
+
+        return response.json(product);
     }
 
     async create(request, response) {
         const { description, user_id } = request.body;
 
-        return response.status(201).json();
+        if (!description)
+            return response.status(400).json({ error: 'Product description can\'t be null' });
+
+        if (user_id === null)
+            return response.status(400).json({ error: 'User ID can\'t be null' });
+
+        const user = await knex('users').where({ id: user_id }).limit(1);
+
+        if (user.length === 0)
+            return response.status(400).json({ error: 'User not found!' });
+
+        const product = await knex('products').insert({ description, user_id }).returning('*');
+
+        return response.status(201).json(product);
     }
 
     async update(request, response) {
         const { id } = request.params;
         const { description } = request.body;
+        const product = await knex('products').where({ id }).limit(1);
+
+        if (product.length === 0)
+            return response.status(400).json({ error: 'Product not found!' });
+
+        if (!description)
+            return response.status(400).json({ error: 'Product description can\'t be null' });
+
+        await knex('products').update({ description }).where({ id });
 
         return response.status(204).send();
     }
 
     async delete(request, response) {
         const { id } = request.params;
+        const product = await knex('products').where({ id }).limit(1);
+
+        if (product.length === 0)
+            return response.status(400).json({ error: 'Product not found!' });
+
+        await knex('products').del().where({ id });
 
         return response.status(204).send();
     }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import ProductController from '../controllers/ProductController';
 
 const router = Router();
 
-router.get('/');
-router.post('/');
-router.get('/:id');
-router.put('/:id');
-router.delete('/:id');
+router.get('/', ProductController.index);
+router.post('/', ProductController.create);
+router.get('/:id', ProductController.find);
+router.put('/:id', ProductController.update);
+router.delete('/:id', ProductController.delete);
 
 export { router };
